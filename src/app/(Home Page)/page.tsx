434b6e81976c8bb31b5da6bc5fb62b3b394b50e5
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-07-02 15:39:14 +0100

    updated

--- src/app/(Home Page)/page.tsx
+++ src/app/(Home Page)/page.tsx
@@ -1,36 +1,30 @@
 "use client"
 import React, { useEffect, useState } from "react";
-import { Metadata } from "next";
-import HeaderSection7 from "@/components/header/HeaderSection7";
-import BannerSection8 from "@/webComponents/banner/BannerSection8";
-import AboutSection6 from "@/components/about/AboutSection6";
-import ServiceSection6 from "@/components/service/ServiceSection6";
-import ServiceSection5 from "@/webComponents/service/ServiceSection5";
-import FunctionSection from "@/components/function/FunctionSection";
-import PricingSection4 from "@/components/pricing/PricingSection4";
-import TestimonialSection5 from "@/webComponents/testimonial/TestimonialSection5";
-import TeamSection5 from "@/webComponents/team/TeamSection5";
-import TeamSection7 from "@/webComponents/team/TeamSection7";
+import dynamic from 'next/dynamic';
+import endpoint from "../../context/endpoint";
 
-import CompareSection from "@/components/compare/CompareSection";
-import ContactSection4 from "@/webComponents/contact/ContactSection4";
-import BlogSection6 from "@/components/blog/BlogSection6";
-import FooterSection4 from "@/components/footer/FooterSection4";
-import AboutSection5 from "@/webComponents/about/AboutSection5";
-import endpoint from "../../context/endpoint"
-
-// export const metadata: Metadata = {
-//   title: "NJC - National Judiciary Council",
-//   description: "National Judiciary Council, Abuja, Nigeria",
-// };
+const HeaderSection7 = dynamic(() => import('@/components/header/HeaderSection7'), { ssr: false });
+const BannerSection8 = dynamic(() => import('@/webComponents/banner/BannerSection8'), { ssr: false });
+const AboutSection6 = dynamic(() => import('@/components/about/AboutSection6'), { ssr: false });
+const ServiceSection6 = dynamic(() => import('@/components/service/ServiceSection6'), { ssr: false });
+const ServiceSection5 = dynamic(() => import('@/webComponents/service/ServiceSection5'), { ssr: false });
+const FunctionSection = dynamic(() => import('@/components/function/FunctionSection'), { ssr: false });
+const PricingSection4 = dynamic(() => import('@/components/pricing/PricingSection4'), { ssr: false });
+const TestimonialSection5 = dynamic(() => import('@/webComponents/testimonial/TestimonialSection5'), { ssr: false });
+const TeamSection5 = dynamic(() => import('@/webComponents/team/TeamSection5'), { ssr: false });
+const TeamSection7 = dynamic(() => import('@/webComponents/team/TeamSection7'), { ssr: false });
+const CompareSection = dynamic(() => import('@/components/compare/CompareSection'), { ssr: false });
+const ContactSection4 = dynamic(() => import('@/webComponents/contact/ContactSection4'), { ssr: false });
+const BlogSection6 = dynamic(() => import('@/components/blog/BlogSection6'), { ssr: false });
+const FooterSection4 = dynamic(() => import('@/components/footer/FooterSection4'), { ssr: false });
+const AboutSection5 = dynamic(() => import('@/webComponents/about/AboutSection5'), { ssr: false });
 
 export default function Home() {
-const [data, setdata] = useState();
-const [gallery, setGallery] = useState([]);
-
-const [mainSlider, setMainSlider] = useState([]);
-const [chiefJudge, setChiefJudge] = useState<any>();
- const [mission, setMission] = useState<{ id: number; msg: string }>({
+  const [data, setdata] = useState();
+  const [gallery, setGallery] = useState([]);
+  const [mainSlider, setMainSlider] = useState([]);
+  const [chiefJudge, setChiefJudge] = useState<any>();
+  const [mission, setMission] = useState<{ id: number; msg: string }>({
     id: 0,
     msg: "",
   });
@@ -40,14 +34,10 @@ const [chiefJudge, setChiefJudge] = useState<any>();
   });
   const [councilMember, setCouncilMember] = useState<any>();
   const [managementStaffs, setManagementStaffs] = useState<any>();
-  const [getNjcProfile, setSetNjcProfile] = useState<{ id: number; summary
-: string }>({
+  const [getNjcProfile, setSetNjcProfile] = useState<{ id: number; summary: string }>({
     id: 0,
-    summary
-: "",
+    summary: "",
   });
-
-
 
   const homePageData = async () => {
     try {
@@ -58,50 +48,34 @@ const [chiefJudge, setChiefJudge] = useState<any>();
       setdata(res.data.data);
       setMainSlider(res.data.data.mainSlider);
       setChiefJudge(res.data.data.chiarmanPicture);
-      setVision(res.data.data.vission)
-      setMission(res.data.data.mission)
-      setCouncilMember(res.data.data.getcouncilMember)
-      setManagementStaffs(res.data.data.getManagementStaff)
-      setSetNjcProfile(res.data.data.getNjcProfile)
+      setVision(res.data.data.vission);
+      setMission(res.data.data.mission);
+      setCouncilMember(res.data.data.getcouncilMember);
+      setManagementStaffs(res.data.data.getManagementStaff);
+      setSetNjcProfile(res.data.data.getNjcProfile);
       setGallery(res.data.data.mediapix);
-      
-
-
     } catch (err) {
       console.log(err);
+    }
   };
-  }
-  
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       homePageData();
-      console.log('Running in the browser')
-    } else {
-      console.log("running in server")
     }
   }, []);
+
   return (
     <main className="overflow-x-hidden car-wash-demo">
       <HeaderSection7 />
-      <BannerSection8 mainSlider={ mainSlider} />
-     {chiefJudge && <TestimonialSection5 chiefJudge={ chiefJudge} getNjcProfile={getNjcProfile}/>} 
-      
-      <ServiceSection5  mission={mission} vission={vision}  />
+      <BannerSection8 mainSlider={mainSlider} />
+      {chiefJudge && <TestimonialSection5 chiefJudge={chiefJudge} getNjcProfile={getNjcProfile} />}
+      <ServiceSection5 mission={mission} vission={vision} />
       <FunctionSection />
-      
-      <TeamSection5   data={ "Current Council Members"} councilMember={councilMember}/>
-     <TeamSection5   data={ "Management Staffs"} councilMember={managementStaffs}/> 
-      {/* <TeamSection7 data={ "Current Council Members"} councilMember={councilMember} />
-      <TeamSection7 data={ "Management Staffs"} councilMember={managementStaffs}  /> */}
-      <AboutSection5 mainSlider={ gallery}/>
-
-
-      {/* <AboutSection6 /> */}
-      {/* <ServiceSection6 /> */}
-      {/* <PricingSection4 /> */}
-      {/* <CompareSection /> */}
-      <ContactSection4  />
+      <TeamSection5 data={"Current Council Members"} councilMember={councilMember} />
+      <TeamSection5 data={"Management Staffs"} councilMember={managementStaffs} />
+      <AboutSection5 mainSlider={gallery} />
+      <ContactSection4 />
       <BlogSection6 />
       <FooterSection4
         style="rv-18-footer"
