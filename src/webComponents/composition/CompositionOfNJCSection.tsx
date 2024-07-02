d2c2f72ab0d1b08a766f0c3a7208d4edbde49197
Uloko John Igbo <johnuloks@gmail.com>
2024-07-02 10:05:52 +0100

    update

--- src/webComponents/composition/CompositionOfNJCSection.tsx
+++ src/webComponents/composition/CompositionOfNJCSection.tsx
@@ -8,7 +8,15 @@ import endpoint from "../../context/endpoint"
 type Props = {
   btnStyle?: string;
 };
-
+interface Blog {
+  id: number;
+  img: string;
+  display_date: string;
+  comments: number;
+  title: string;
+  placeby: string
+  content: string
+}
 const CompositionOfNJCSection = ({ btnStyle }: Props) => {
 
   const [data, setdata] = useState();
@@ -19,6 +27,7 @@ const CompositionOfNJCSection = ({ btnStyle }: Props) => {
   });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [allNews, setAllNews] = useState<Blog[]>([]);
   
   const fetchCompositionData = async () => {
     try {
@@ -27,14 +36,27 @@ const CompositionOfNJCSection = ({ btnStyle }: Props) => {
       setdata(res.data.data);
       setCompositionData(res.data.data.getComposition);
       setLoading(false);
-    } catch (err) {
+    } catch (err:any) {
       setError(err);
       setLoading(false);
+    }
+  };
+  const getAllNews = async () => {
+    try {
+      const res = await endpoint.get("/news");
+      if (Array.isArray(res.data.data.getNews)) {
+        setAllNews(res.data.data.getNews);
+      } else {
+        console.error("API response is not an array");
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
   
   useEffect(() => {
     fetchCompositionData();
+    getAllNews();
   }, []);
   
   if (loading) return <div>Loading...</div>;
@@ -47,13 +69,21 @@ const CompositionOfNJCSection = ({ btnStyle }: Props) => {
           <div className="col-lg-8">
             <div className="rv-blog-details-left">
               <h2 className="rv-blog-details__title">COMPOSITION OF NJC</h2>
-                <div dangerouslySetInnerHTML={{ __html: compositionData.composition }} />
+                <div 
+                  style={{
+                    textAlign: "justify",
+                    textJustify: "inter-word",
+                    width: "100%",
+                    margin: "auto",
+                    maxWidth: "100%",
+                  }}
+                dangerouslySetInnerHTML={{ __html: compositionData.composition }} />
             </div>
           </div>
 
           <div className="col-lg-4 col-md-8 col-10 col-xxs-12">
 
-            <BlogRecentSection />
+            <BlogRecentSection allNews={allNews} />
           </div>
         </div>
       </div>
